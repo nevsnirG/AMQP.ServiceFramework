0cd1a577f7cc480ad5bc897bd31acbccf2a272f6
nevsnirG <14148970+nevsnirG@users.noreply.github.com>
2020-03-30 22:22:03 +0200

    Added a few unit tests for the CommandHandlerContextFactory.

---
+++ src/AMQP.ServiceFramework.Tests/CommandHandlerContextFactoryTests.cs
@@ -0,0 +1,72 @@
+﻿using AMQP.ServiceFramework.Attributes;
+using AMQP.ServiceFramework.Factories;
+using Moq;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Xunit;
+
+namespace AMQP.ServiceFramework.Tests
+{
+    public class CommandHandlerContextFactoryTests
+    {
+        private readonly Mock<MethodInfo> _methodInfoMock;
+
+        private readonly Expression<Func<MethodInfo, ParameterInfo[]>> _getParametersMock;
+
+        //private readonly Expression<Func<MethodInfo, TopicSubscriptionAttribute>> 
+
+        public CommandHandlerContextFactoryTests()
+        {
+            _methodInfoMock = new Mock<MethodInfo>(MockBehavior.Strict);
+
+            _getParametersMock = (method) => method.GetParameters();
+        }
+
+        [Fact]
+        public void Create_NullInput_ArgumentNullException()
+        {
+            //Arrange
+            var commandHandlerContextFactory = new CommandHandlerContextFactory();
+
+            try
+            {
+                //Act
+                commandHandlerContextFactory.Create(null);
+                Assert.True(false, "The method was supposed to throw an ArgumentNullException.");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.Equal("methodInfo", ex.ParamName);
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void Create_MethodInfo_InvalidParameterCount(int count)
+        {
+            //Arrange
+            _methodInfoMock.Setup(_getParametersMock)
+                .Returns(new ParameterInfo[count]);
+            var commandHandlerContextFactory = new CommandHandlerContextFactory();
+
+            try
+            {
+                //Act
+                commandHandlerContextFactory.Create(_methodInfoMock.Object);
+
+                //Assert
+                Assert.True(false, "The method was supposed to throw an ArgumentException.");
+            }
+            catch (ArgumentException ex)
+            {
+                _methodInfoMock.Verify(_getParametersMock);
+                Assert.Equal("methodInfo", ex.ParamName);
+                Assert.StartsWith("The specified method can only have 1 parameter.", ex.Message);
+            }
+        }
+    }
+}
--- src/AMQP.ServiceFramework.Tests/ServiceBuilderTests.cs
+++ src/AMQP.ServiceFramework.Tests/ServiceBuilderTests.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Moq.Protected;
 using Xunit;
